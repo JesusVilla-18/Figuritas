b325bc44f6531d0aa981521f871884346bc0d6d1
JesusVilla-18 <90075221+JesusVilla-18@users.noreply.github.com>
2026-04-28 23:29:03 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,38 @@
-const CACHE = "album-cache-v5";
+const CACHE = "album-cache-v6";
 
 self.addEventListener("install", e=>{
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(cache=>{
       return cache.addAll([
+        "./",
         "index.html"
       ]);
     })
   );
 });
 
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", e=>{
+
+  const url = e.request.url;
+
+  // 🔥 NO CACHEAR API (esto era tu bug)
+  if(url.includes("script.google.com")){
+    return e.respondWith(fetch(e.request));
+  }
+
+  // cache-first para estáticos
   e.respondWith(
     caches.match(e.request).then(res=>{
       return res || fetch(e.request);
